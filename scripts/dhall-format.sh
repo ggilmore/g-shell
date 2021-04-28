b8e8dc36aa787bb551814066bfb698c963722d15
Geoffrey Gilmore <ggilmore@geoffresonalmbp.lan>
2021-04-27 21:57:07 -0700

    hopefully fix edited script

--- scripts/dhall-format.sh
+++ scripts/dhall-format.sh
@@ -1,38 +1,26 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
-set -euo pipefail
+set -eu pipefail
 
 DHALL_FILES=()
+
 mapfile -t DHALL_FILES < <(scripts/ls-dhall-files.sh)
 
-# https://unix.stackexchange.com/a/256201
-function my_chronic() {
-  tmp=$(mktemp) || return # this will be the temp file w/ the output
-  "$@" >"$tmp" 2>&1       # this should run the command, respecting all arguments
-  ret=$?
-  [ "$ret" -eq 0 ] || cat "$tmp" # if $? (the return of the last run command) is not zero, cat the temp file
-  rm -f "$tmp"
-  return "$ret" # return the exit status of the command
-}
-
-export -f my_chronic
-
-function check() {
+function format() {
   local file="$1"
 
-  local CHECK_ARGS=(
-    "dhall"
+  local FORMAT_ARGS=(
+    "format"
+    "--inplace"
+    "${file}"
   )
 
-  if [[ "${CI:-"false"}" == "true" ]] || [[ "${DHALL_CHECK_EXPLAIN:-"false"}" == "true" ]]; then
-    CHECK_ARGS+=("--explain")
+  if [ "${CHECK:-"false"}" == "true" ]; then
+    FORMAT_ARGS+=("--check")
   fi
 
-  CHECK_ARGS+=("--file")
-  CHECK_ARGS+=("${file}")
-
-  result=$(my_chronic "${CHECK_ARGS[@]}" 2>&1)
+  result=$(dhall "${FORMAT_ARGS[@]}" 2>&1)
   rc=$?
 
   if [ -n "$result" ]; then
@@ -41,8 +29,8 @@ function check() {
     echo
   fi
 
-  return "$rc"
+  exit "$rc"
 }
-export -f check
+export -f format
 
-./scripts/parallel_run.sh check {} ::: "${DHALL_FILES[@]}"
+./scripts/parallel_run.sh format {} ::: "${DHALL_FILES[@]}"
